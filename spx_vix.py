cb5bc7ca1eae13d2602313391ea54a4074789c26
ywaterhouse <136723116+ywaterhouse@users.noreply.github.com>
2023-07-10 10:04:05 -0500

    my commit

--- spx_vix.py
+++ spx_vix.py
@@ -13,62 +13,66 @@ import statsmodels.api as sm
 spx_df = pd.read_csv(r'\\lfp-chi.local\shared\Lakeview_Shared\Lakeview Investment Group\Yoselyn\spx_index.csv') 
 vix_df = pd.read_csv(r'\\lfp-chi.local\shared\Lakeview_Shared\Lakeview Investment Group\Yoselyn\VIX_Futures_Minutely.csv')
 
-def clean_df(df,index,index1,index2): 
-    df[index1] = df[index].dt.date 
-    df[index2] = df[index2].replace(0, np.nan) 
-    
-def df_resample(df,index): 
-    df = df.set_index(df[index]) 
+def clean_df(quote_datetime, close):  
+    date_series = quote_datetime.dt.date 
+    close = close.replace(0, np.nan)  
+    return date_series,close
+
+def df_resample(df,quote_datetime): 
+    df = df.set_index(quote_datetime) 
     df = df.resample('15T').first()
     return df 
     
-def remove_outliers(df,index):
-    ret_df = df[index].loc[(df[index]>df[index].mean()-df[index].std()*3)&(df[index]<df[index].mean()+df[index].std()*3)] 
-    return ret_df
+def remove_outliers(returns):
+    spx1_ret = returns.loc[(returns>returns.mean()-returns.std()*3)&(returns<returns.mean()+returns.std()*3)] 
+    return spx1_ret
 
-def beta_linear(index,index1):
-    X=merge_df[index] 
-    Y=merge_df[index1] 
+def beta_linear(returns,returns1):
+    X=returns 
+    Y=returns1
     plot.scatter(X,Y)
     X = sm.add_constant(X) 
     mod = sm.OLS(Y,X).fit() 
     return mod 
 
-def beta_covariance(index,index1):
-    D=merge_df[index] 
-    E=merge_df[index1] 
+def beta_covariance(returns,returns1):
+    D=returns
+    E=returns1
     betas = np.cov(E,D)[0][1]/np.var(D)
     return betas
 
 def opt_bins(returns):
     bin_num = int(np.log2(returns.count())+1)
     return bin_num 
+
+def plot_returns(returns):
+    returns.plot.hist(bins = opt_bins(returns))
     
 """SPX_DF""" 
-#change type to then make time zone the same as vix  
+
 spx_df['quote_datetime'] = pd.to_datetime(spx_df.quote_datetime) 
 spx_df['quote_datetime'] = spx_df.quote_datetime.dt.tz_localize('US/Eastern').dt.tz_convert('US/Central').dt.tz_localize(None) 
-spx15_df = spx_df.copy()   
+spx15_df = spx_df.copy()     
 #add date and remove nan values to successfully calculate returns, calc returns, and remove outliers 
-clean_df(spx_df,'quote_datetime','date','close')
+spx_df['date'],spx_df['close']=clean_df(spx_df['quote_datetime'],spx_df['close'])
 spx_df['spx_returns'] = spx_df.groupby('date')['close'].apply(lambda x : np.log(x) - np.log(x.shift(1))) 
-spx1_ret = remove_outliers(spx_df,'spx_returns')  
-spx1_ret.plot.hist(bins = opt_bins(spx1_ret))
+spx1_ret = remove_outliers(spx_df.spx_returns)                                   
+plot_returns(spx1_ret)
 
-"""SPX2_DF""" 
+"""SPX15_DF""" #change 15 min df name
 #set index to datetime to resample to 15 minute intervals 
-spx15_df = df_resample(spx15_df,'quote_datetime') 
+spx15_df = df_resample(spx15_df,spx15_df['quote_datetime']) 
 #add date and remove nan values to successfully calculate returns, calc returns, and remove outliers 
-clean_df(spx15_df,'quote_datetime','date','close')
+spx15_df['date'],spx15_df['close'] = clean_df(spx15_df['quote_datetime'],spx15_df['close'])
 spx15_df['spx_returns'] = spx15_df.groupby('date')['close'].apply(lambda x : np.log(x) - np.log(x.shift(1)))
-spx15_ret = remove_outliers(spx15_df,'spx_returns')
-spx15_ret.plot.hist(bins = opt_bins(spx15_ret))
-
+spx15_ret = remove_outliers(spx15_df.spx_returns)
+plot_returns(spx15_ret)
+ 
 """VIX_DF""" 
 #making into 15 min interval
 vix_df["quote_datetime"] = pd.to_datetime(vix_df.quote_datetime) 
 #set index to datetime to resample to 15 minute intervals for fair return comparison
-vix_df = df_resample(vix_df,'quote_datetime') 
+vix_df = df_resample(vix_df,vix_df['quote_datetime']) 
 #drop any nans to successfully sort vix_df 
 vix_df = vix_df.dropna(how = 'all') 
 vix_df = vix_df.sort_values(by=['trade_date', 'expiration'], ascending = True) 
@@ -82,22 +86,19 @@ vix_df['exp_month'] = np.searchsorted(vix_exps,vix_df.expiration) - np.searchsor
 #locate on front months, calculate returns, and remove outliers to later correctly calcuate beta
 vix_df = vix_df.loc[vix_df.exp_month == 1] 
 vix_df['vix_returns'] = np.log(vix_df.close) - np.log(vix_df.close.shift(1)) 
-vix_ret = remove_outliers(vix_df,'vix_returns')
-vix_ret.plot.hist(bins = opt_bins(vix_ret))
+vix_ret = remove_outliers(vix_df.vix_returns)
+plot_returns(vix_ret)
 
 """Calc Beta Linear Regression"""
 #get rid of duplicate quote_dateime to merge then merge
 spx15_df = spx15_df.drop('quote_datetime',axis=1) 
 vix_df = vix_df.drop('quote_datetime',axis=1) 
-merge_df = spx15_df.merge(vix_df, right_on= 'quote_datetime',left_on = 'quote_datetime') 
+merge_df = spx15_df.merge(vix_df, how= 'left', right_on= 'quote_datetime',left_on = 'quote_datetime')
 #remove nan values to successfully make scatter plot then plot 
 merge_df = merge_df.dropna(subset = ['vix_returns']) 
 merge_df = merge_df.dropna(subset = ['spx_returns']) 
-model  = beta_linear('spx_returns','vix_returns')
+model  = beta_linear(merge_df['spx_returns'],merge_df['vix_returns'])
 
 """Calc beta using Covariance"""
-cov_calc = beta_covariance('spx_returns','vix_returns')
-
-
-
+cov_calc = beta_covariance(merge_df['spx_returns'],merge_df['vix_returns']) 
 
